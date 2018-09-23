24a7b8d648baee51981610518dbcf74a954648c9
Yelmukalmas <160104023@stu.sdu.edu.kz>
2018-09-24 01:36:21 +0600

    First C program

---
+++ HelloC.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+        printf("My First Program in C Lg!\n");
+        printf("МОй Первая программа на языке Си!\n");
+        //kflasdaslka
+        /*asdklasdj;kakdlas;d*/
+}
